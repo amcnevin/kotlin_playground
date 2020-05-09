2d8ae4cb277294cced22a36892371a49ccbabdb2
amcnevin <amcnevin@gmail.com>
2020-05-08 23:17:24 -0500

    more chapter 2

--- src/main/kotlin/chapter_2/chap2_script.kts
+++ src/main/kotlin/chapter_2/chap2_script.kts
@@ -1,3 +1,23 @@
-import chapter_2.max
+import chapter_2.*
 
+// test out functions
 println(max(1,2))
+println(max(100, 2000))
+
+println(min(1, 100))
+
+// testing bounds of vals and vars
+println(ten)
+println(eleven)
+
+//ten = 11, ten is an immutable reference.
+eleven = 12
+
+println(ten)
+println(eleven)
+
+// eleven = "New String", cannot change the type of the reference
+// statically typed!
+
+// String Templates
+println("I can count to $ten but not $eleven")
--- src/main/kotlin/chapter_2/main.kt
+++ src/main/kotlin/chapter_2/main.kt
@@ -4,6 +4,16 @@ fun main(args: Array<String>) {
     println("Hello, World!")
 }
 
-fun max(a: Int, b: Int): Int {
-    return if (a > b) a else b
+// using an expression body
+fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+// using a block body
+fun min(a: Int, b: Int): Int {
+    return if (a < b) a else b
 }
+
+// test of val and var
+val ten = 10
+var eleven = 11
+
+// why not make val's const?
